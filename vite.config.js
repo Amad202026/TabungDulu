8a2d2a690b2e4ae4e86e4e6656f59ae3f7f70cb4
Mino110 <zklt9941@gmail.com>
2026-06-13 18:40:34 +0800

    Update 7

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'TabungDulu',
+        short_name: 'TabungDulu',
+        description: 'Aplikasi catatan keuangan & target tabungan',
+        theme_color: '#0B0F0E',
+        background_color: '#0B0F0E',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/.*\.supabase\.co\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'supabase-cache',
+              networkTimeoutSeconds: 5,
+              cacheableResponse: { statuses: [0, 200] },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+})
